cf38d3b6114a0e2f940cef493bc00bd56f7ec5c9
Dasffafa <1605302380@qq.com>
2023-07-23 11:45:54 +0800

    Initial commit

---
+++ src/main/java/com/dasffafa/SquareDancer.java
@@ -0,0 +1,9 @@
+package com.dasffafa;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(SquareDancer.MOD_ID)
+public class SquareDancer {
+    public static final String MOD_ID = "square_dancer";
+
+}
